7206f7c50b5062f6e3b754e5f111cd248f1faa1d
Mora Prathap Kumar Reddy <moraprathapkumarreddy1@email.com>
2022-01-31 16:07:28 +0530

    my day1 java prgs

---
+++ Day1/javademo/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package javademo;
+
+public class EvenOrOdd {
+	public static void main(String args[]) {
+		int num=9;
+		if(num % 2== 0) {
+			System.out.println("Enter number is even");
+		}
+		else {
+			System.out.println("Enter number is odd");
+		}
+	}
+
+
+}
